fb0862a80af8fee4551d4f092115f4fbf4e14193
Lex <kasatkin-11@yandex.ru>
2020-07-09 15:09:47 +0300

    Change direction of buttons
    Add adaptive margin for desktop and mobile.
    Change icon of main float button.

--- src/components/Phone.vue
+++ src/components/Phone.vue
@@ -2,12 +2,12 @@
     <v-layout class="float-button-container">
         <v-flex>
             <v-speed-dial
-                    class="float-button"
+                    :class="[floatButtonMobile ? 'float-button-mobile' : 'float-button-desktop']"
                     v-model="fab"
                     bottom
                     right
                     fixed
-                    direction="left"
+                    direction="top"
                     open-on-hover
                     transition="scale-transition"
             >
@@ -18,13 +18,13 @@
                             dark
                             fab
                     >
-                        <v-icon>mdi-home</v-icon>
+                        <v-icon>mdi-phone-message</v-icon>
                     </v-btn>
                 </template>
                     <v-btn
                             fab
                             dark
-                            large
+                            small
                             color="green"
                             :href="mailLink"
                     >
@@ -33,7 +33,7 @@
                     <v-btn
                             fab
                             dark
-                            large
+                            small
                             color="indigo"
                             :href="telLink"
                     >
@@ -67,6 +67,10 @@
 
             telLink(){
                 return 'tel:209-51-71';
+            },
+
+            floatButtonMobile(){
+                return this.$vuetify.breakpoint.name === 'xs' || this.$vuetify.breakpoint.name === 'sm';
             }
         }
     }
@@ -79,7 +83,12 @@
         z-index: 100;
     }
 
-    .float-button{
+    .float-button-mobile{
+        margin-bottom: 64px;
+        margin-right: 32px;
+    }
+
+    .float-button-desktop{
         margin-bottom: 128px;
         margin-right: 64px;
     }
